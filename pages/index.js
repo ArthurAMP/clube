56847b0d73b2418969911c489fcfe6c6eaa9e6c3
ArthurAMP <arthurmpapp@gmail.com>
2021-08-15 16:13:03 -0300

    added navbar

--- pages/index.js
+++ pages/index.js
@@ -8,12 +8,16 @@ export default function Home() {
       <Head>
         <title>Clube de programação</title>
       </Head>
-
+      <div className={styles.navbar}>
+        <a href="#inicio" className={styles.navitems}>inscreva-se</a>
+        <a href="#sobre" className={styles.navitems}>sobre</a>
+        <a href="#faq" className={styles.navitems}>faq</a>
+        <a href="#contato" className={styles.navitems}>contato</a>
+      </div>
       <body>
         <h1>Clube de programação</h1>
         <h2>O clube</h2>
       </body>
-
     </div>
   )
 }
