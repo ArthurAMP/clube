00240496874061ec6f40be0a40aef9997ee03fba
ArthurAMP <arthurmpapp@gmail.com>
2021-08-16 18:56:44 -0300

    fixed navbar

--- pages/index.js
+++ pages/index.js
@@ -4,17 +4,36 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
+    
     <div className={styles.container}>
-      <Head>
-        <title>Clube de programação</title>
-      </Head>
+      <head>
+        
+      </head>
       <div className={styles.navbar}>
         <a href="#inicio" className={styles.navitems}>inscreva-se</a>
         <a href="#sobre" className={styles.navitems}>sobre</a>
         <a href="#faq" className={styles.navitems}>faq</a>
         <a href="#contato" className={styles.navitems}>contato</a>
       </div>
-      <body>
+      <body className={styles.content}>
+        <h1>Clube de programação</h1>
+        <h2>O clube</h2>
+        <h1>Clube de programação</h1>
+        <h2>O clube</h2>
+        <h1>Clube de programação</h1>
+        <h2>O clube</h2>
+        <h1>Clube de programação</h1>
+        <h2>O clube</h2>
+        <h1>Clube de programação</h1>
+        <h2>O clube</h2>
+        <h1>Clube de programação</h1>
+        <h2>O clube</h2>
+        <h1>Clube de programação</h1>
+        <h2>O clube</h2>
+        <h1>Clube de programação</h1>
+        <h2>O clube</h2>
+        <h1>Clube de programação</h1>
+        <h2>O clube</h2>
         <h1>Clube de programação</h1>
         <h2>O clube</h2>
       </body>
