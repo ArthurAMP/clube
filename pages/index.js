8b143f3d6dfac2dfe804f5e1fdcbbb7ee217297f
Lawrence Melo <lawrencefmm@gmail.com>
2021-09-06 19:13:05 -0300

    home page

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,35 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
+
+import heroBg from '../public/bruh2.jpg'
 
 export default function Home() {
   return (
     <div className={styles.container}>
+
       <Head>
         <title>Clube de programação</title>
       </Head>
-      <div className={styles.navbar}>
-        <a href="#inicio" className={styles.navitems}>inscreva-se</a>
-        <a href="#sobre" className={styles.navitems}>sobre</a>
-        <a href="#faq" className={styles.navitems}>faq</a>
-        <a href="#contato" className={styles.navitems}>contato</a>
+
+      <div className={styles.heroBackground}>
+
+        <div className={styles.background}>
+          <Image src={heroBg} alt="background" />
+        </div>
+
+        <div className={styles.textArea}>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore  ut labore et dolore  ut labore et dolore  ut labore et dolore lorem ipsum dolor.</p>
+          <div className={styles.btn}>
+            <Link href="/"><a>Quero me inscrever!</a></Link>
+          </div>
+        </div>
+
       </div>
-      <body>
-        <h1>Clube de programação</h1>
-        <h2>O clube</h2>
-      </body>
+
+
+
     </div>
   )
 }
