ea5440bfc6fbddd7e2260a195f6bc0f6985cf444
Lawrence Melo <lawrencefmm@gmail.com>
2021-09-21 13:04:20 -0300

    responsive text box

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 import Navbar from './views/navbar'
 import Landing from './views/landing'
+
+import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
 
     <div className={styles.container}>
+
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
 
       <Head>
         <title>Clube de programação</title>
